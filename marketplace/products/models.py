87b84f77c9f18b63dca4acf3efa0806b2b139b52
Ivan <movmogilev@gmail.com>
2026-03-22 21:32:59 +0300

    Добавлены несколько новыз страниц, отображенийЮ создана модель

--- marketplace/products/models.py
+++ marketplace/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    content = models.TextField(verbose_name='Описание товара')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
--- marketplace/products/views.py
+++ marketplace/products/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, UpdateView, DeleteView, CreateView
+from .forms import ProductForm
+from .models import Product
+from django.urls import reverse_lazy
 
-# Create your views here.
+def product_home(request):
+    products = Product.objects.all()
+    return render(request,'products/product-home.html',{'products': products})
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product-detail.html'
+    context_object_name = ('product')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'products/create.html'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/product-delete.html'
+    success_url = reverse_lazy('product-home')
+
+def create(request):
+    error = None
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product-home')
+        else:
+            error = 'Form is not valid'
+    else:
+        form = ProductForm()
+    return render(request, 'products/create.html', {'form': form, 'error': error})
