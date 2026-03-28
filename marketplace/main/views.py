aa9e8f97521e2de61edfb8f7fc2f277bf34a1300
Ivan <movmogilev@gmail.com>
2026-03-28 16:27:33 +0300

    Добавлено отслеживание сессий, реализован демонстративный вывод этой информации

--- marketplace/main/views.py
+++ marketplace/main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/home_page.html')
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_visits': num_visits + 1,
+    }
+    return render(request, 'main/home_page.html', context)
 
 def about(request):
     return render(request, 'main/about.html')
